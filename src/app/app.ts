3a16eceb3421ef9feec717379b97afe7cee510f8
Yann-Olympe <yannmboh@gmail.com>
2026-06-30 01:48:50 +0200

    header component - affichage du compteur

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Restaurant } from './models/restaurant';
 import { Header } from './components/header/header';
 import { RestaurantCard } from './components/restaurant-card/restaurant-card';
 import { RestaurantList } from './components/restaurant-list/restaurant-list';
+import { RatingEvent } from './models/ratingEvent';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,RestaurantCard,RestaurantList],
+  imports: [RouterOutlet,Header,RestaurantList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -20,5 +21,20 @@ export class App {
        { id: 4, name: 'Saveurs du Wouri', district: 'Bonamoussadi',     specialty: 'Poisson braisé', currentRating: 0 },
        { id: 5, name: "L'Akwa Gourmand", district: 'Akwa',     specialty: 'Bobolo et sauce arachide', currentRating: 0 },
        { id: 6, name: 'Le Royal de Bali', district: 'Bali',     specialty: 'Koki et plantain', currentRating: 0 } ]); 
+    
+      ratedCount = computed(() => 
+      this.restaurants().filter(r => r.currentRating > 0).length);
+
+
+       onRestaurantRated(value:RatingEvent){
+      this.restaurants.update(liste => 
+        liste.map(r => 
+          r.id===value.restaurantId?{
+            ...r,currentRating : value.newRating
+          }:r
+        )
+      )
+
+    }
 
 }
--- src/app/components/restaurant-list/restaurant-list.ts
+++ src/app/components/restaurant-list/restaurant-list.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { RestaurantCard } from '../restaurant-card/restaurant-card';
+import { Restaurant } from '../../models/restaurant';
+import { RatingEvent } from '../../models/ratingEvent';
 
 @Component({
   selector: 'app-restaurant-list',
@@ -7,4 +9,12 @@ import { RestaurantCard } from '../restaurant-card/restaurant-card';
   templateUrl: './restaurant-list.html',
   styleUrl: './restaurant-list.css',
 })
-export class RestaurantList {}
+export class RestaurantList {
+  restaurants = input<Restaurant[]>();// recois la liste des info depuis app
+  restaurantRated = output<RatingEvent>();
+
+  onRestaurantRated(value:RatingEvent){
+    this.restaurantRated.emit(value); // propagation de la note attribuer depuis le starRating
+  }
+
+}
